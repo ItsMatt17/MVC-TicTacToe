d91a8598a7a59c52e72b41ebd73d17dcffdf78ed
ItsMatt17 <mattspycrafter@gmail.com>
2025-11-17 22:59:01 -0500

    Added Game interface to allow different game types such as Player vs Player or Player vs AI

---
+++ src/model/Player.java
@@ -0,0 +1,40 @@
+package model;
+
+public class Board {
+
+    private final int[] winBoards = {0b000000111, 0b000111000, 0b111000000, 0b100100100, 0b010010010, 0b001001001, 0b100010001, 0b001010100};
+    private int board = 0b000000000;
+    private final char boardChar;
+
+    public Board(char c){
+        this.boardChar = c;
+    }
+
+    private void setPos(int pos){
+        board |= 1 << pos;
+    }
+
+    public boolean isWon() {
+        for (int mask : winBoards) {
+            if (mask == board) return true;
+        }
+        return false;
+    }
+    // Assuming the position is already free
+    public void play(int pos){
+        setPos(pos);
+    }
+    // minimax this
+    public void play(){
+    }
+
+    public void minimax(){
+
+    }
+
+
+
+
+
+
+}
